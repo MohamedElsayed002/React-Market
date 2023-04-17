438d20ec8855ccc4b8f6c2b1460abf78154462c1
Mohamed Elsayed Ali <mohamedelsayed20258@gmail.com>
2023-04-17 20:38:24 +0200

    product-component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,27 @@
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
+import Product from "./components/Product/Product"
+
+
+import data from './data.json'
+
+import {useState} from 'react'
+
+
 function App() {
+
+  const [products,setProducts] = useState(data)
 
   return (
     <div className="App">
       <div className="layout">
         <Header/>
-        <main>d</main>
+        <main>
+          <div className="wrapper">
+              <Product products={products}/>
+            <div className="filterEl">Filter</div>
+          </div>
+        </main>
         <Footer/>
       </div>
     </div>
