5b37488c147b0dd1a3fefacc652e1f77a2686bf8
Mohamed Elsayed Ali <mohamedelsayed20258@gmail.com>
2023-04-17 16:55:21 +0200

    starter react project

---
+++ client/gulpfile.js
@@ -0,0 +1,12 @@
+
+
+import gulp from 'gulp'
+import gulpSass from 'gulp-sass'
+import sass from 'sass'
+
+
+let sassGulp = gulpSass(sass)
+
+gulp.task('sass' , async function () {
+    gulp.src('./src/index.scss').pipe(sassGulp()).pipe(gulp.dest('./src/css'))
+})
---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import image1 from '../images/image1.jpg'
+function App() {
+
+  return (
+    <img width={'200px'} src={image1} alt="photo"  />
+  )
+}
+
+export default App
